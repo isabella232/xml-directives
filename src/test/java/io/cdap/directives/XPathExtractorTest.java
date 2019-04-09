765232b6adea296eba7e76ce4e3333d9281ad334
sagarkapare <sagarkapare@google.com>
2019-04-09 15:32:57 -0700

    Update for 6.0.0

--- src/test/java/io/cdap/directives/XPathExtractorTest.java
+++ src/test/java/io/cdap/directives/XPathExtractorTest.java
@@ -1,10 +1,26 @@
-package co.cask.directives;
+/*
+ *  Copyright © 2019 CDAP
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ *  use this file except in compliance with the License. You may obtain a copy of
+ *  the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing permissions and limitations under
+ *  the License.
+ */
 
-import co.cask.wrangler.api.RecipePipeline;
-import co.cask.wrangler.api.Row;
-import co.cask.wrangler.test.TestingRig;
-import co.cask.wrangler.test.api.TestRecipe;
-import co.cask.wrangler.test.api.TestRows;
+package io.cdap.directives;
+
+import io.cdap.wrangler.api.RecipePipeline;
+import io.cdap.wrangler.api.Row;
+import io.cdap.wrangler.test.TestingRig;
+import io.cdap.wrangler.test.api.TestRecipe;
+import io.cdap.wrangler.test.api.TestRows;
 import org.junit.Assert;
 import org.junit.Test;
 
